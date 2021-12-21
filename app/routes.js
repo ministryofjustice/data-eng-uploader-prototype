3cea40a827e5615c9399b7a051644c38591445e0
SoumayaMauthoorMOJ <soumaya.mauthoor@digital.justice.gov.uk>
2021-12-21 15:36:58 +0000

    updated html pages following review

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.post('/database_answer', function (req, res) {
+    
+    var databaseType = req.session.data['database_type']
+    if (databaseType == "existing"){
+      res.redirect('/table_name')
+    } else {
+      res.redirect('/table_name_new')
+    }
+  })
 
 module.exports = router
